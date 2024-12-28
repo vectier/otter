d207d7f240609757b9f44822fccec92a0d64225c
riflowth <riflowth@gmail.com>
2024-12-28 16:02:51 +0700

    refactor: improve get file logging

--- pkg/api/seaweedfs.go
+++ pkg/api/seaweedfs.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/bmatcuk/doublestar/v4"
 	"github.com/go-chi/chi/v5"
@@ -10,25 +11,30 @@ import (
 )
 
 func (s *server) GetFile(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+
 	claims, err := auth.GetClaims(r)
 	if err != nil {
-		log.Err(err).Msg("cannot get claims")
+		log.Error().Err(err).Msg("cannot get claims")
 		response(w, http.StatusUnauthorized, "unauthorized")
 		return
 	}
 
 	path := chi.URLParam(r, "*")
+	logger := log.With().Str("path", path).Str("client", claims.Client).Logger()
 
 	for _, glob := range claims.GrantedDirectories {
 		match, err := doublestar.Match(glob, path)
 		if err != nil {
-			log.Err(err).Msg("invalid match pattern from granted directories")
+			logger.Error().Err(err).Str("glob", glob).Msg("failed to match pattern of granted directories")
 			response(w, http.StatusInternalServerError, "invalid granted directories")
 			return
 		}
 		if match {
 			if err := s.sc.PipeFile(r.Context(), path, w); err != nil {
-				log.Err(err).Msg("cannot pipe file")
+				logger.Error().Err(err).Msg("cannot pipe file")
+			} else {
+				logger.Debug().Str("path", path).Dur("execTime", time.Since(start)).Msg("pipe file")
 			}
 			return
 		}
